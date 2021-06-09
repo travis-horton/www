0788adf236e7388d2571aced6e0fa9df413ad2a8
kiddspazz <travis@travish.com>
2021-06-08 21:59:16 -0600

    Add styles specific to header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,48 +1,60 @@
 import React from 'react';
-import Image from '/src/sharedComponents/Image';
+import styled from 'styled-components';
 
 import headshotThumb from 'url:/src/assets/media/headshot-thumb.png';
 import headshot from 'url:/src/assets/media/headshot.png';
 import monkeyThumb from 'url:/src/assets/media/monkeh-thumb.jpg';
 import monkey from 'url:/src/assets/media/monkeh.jpg';
 
+import Image from '/src/sharedComponents/Image';
 import {
+  Main,
   StyledH1,
+  StyledH2,
 } from '/src/sharedComponents/units';
 
+const ProfilePic = styled.div`
+  float: left;
+  margin-right: 14px;
+`;
+
+const MonkeyPic = styled.div`
+  float: right;
+  margin-left: 14px;
+`;
+
 const Home = () => (
-  <div>
-    <div>
+  <Main>
+    <ProfilePic>
       <Image alt="headshot" thumb={headshotThumb} src={headshot} height={285} width={285} />
-    </div>
+    </ProfilePic>
     <header>
       <div>
         <StyledH1>
           travis horton
         </StyledH1>
-        <h2>
+        <StyledH2>
           software engineer
           <br />
           pianist
           <br />
           accountant
-        </h2>
+        </StyledH2>
       </div>
     </header>
     <div>
       <p>
         i&apos;m travis. i program in react/javascript/css/html and i&apos;ve been trying
-        (unsuccessfully) to learn rust&mdash;there&apos;s just not enough time!! i play the
-        piano. i do some accounting on the side. i live in boise, idaho with my wonderful
-        wife anne and our cat monkey. and i run. really slowly.
+        to learn rust. i play the piano. i do some accounting on the side. i live in boise, idaho
+        with my wonderful wife anne and our cat monkey. and i run. really slowly.
       </p>
       <p>
-        we bought a house in the summer of 2020! what an adult thing to do&mdash;i
+        we bought a house in the summer of 2020! what an adult thing to do &mdash; i
         definitely never thought i&apos;d get this responsible.
       </p>
-      <div>
+      <MonkeyPic>
         <Image alt="monkey" thumb={monkeyThumb} src={monkey} height={210} width={157} />
-      </div>
+      </MonkeyPic>
       <p>
         i attended the recurse center spring 1, 2019 batch: february 18 - may 9. it was a
         great time, i met a lot of amazing people, and i learned a ton. here are some of
@@ -57,12 +69,12 @@ const Home = () => (
       <p>
         i studied piano for 6 years at unc-school of the arts and 2 more years at the new
         england conservatory. i moved to new york city in 2011 and worked as a pianist
-        there for 6 years&mdash;played carnegie hall, joe&apos;s pub, off-broadway shows,
+        there for 6 years &mdash; played carnegie hall, joe&apos;s pub, off-broadway shows,
         worked for nyu and the brooklyn youth chorus as a vocal coach and staff pianist. i
         rode my bicycle a lot.
       </p>
     </div>
-  </div>
+  </Main>
 );
 
 export default Home;
--- src/sharedComponents/units/h1.js
+++ src/sharedComponents/units/h1.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 
 const StyledH1 = styled.h1`
   color: #1d0a00;
-  font-family: sans-serif;
+  font-family: 'arkhip', sans-serif;
   font-size: 40px;
+  letter-spacing: 0.5px;
+  margin: 0;
+  text-transform: uppercase;
 `;
 
-export default StyledH1
+export default StyledH1;
