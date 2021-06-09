097aae6dfc6b5e836a86b4e8b6f5cf5b64d4f26c
kiddspazz <travis@travish.com>
2021-06-09 17:43:11 -0600

    Add anchor styles

---
+++ src/sharedComponents/units/a.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledA = styled.a`
+  text-decoration: none;
+  color: #a714a2;
+
+  &:visited {
+    box-sizing: border-box;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default StyledA;
