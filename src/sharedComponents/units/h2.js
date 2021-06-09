0f718149dcd1145e15a8fce89d1faf7c332ca499
kiddspazz <travis@travish.com>
2021-06-09 17:34:34 -0600

    Update styles in /programming

--- src/sharedComponents/units/h2.js
+++ src/sharedComponents/units/h2.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const StyledH2 = styled.h2`
   color: #1d0a00;
   font-size: 22px; letter-spacing: 0.5px;
-  margin: 0;
+  margin: 10px 0 8px 0;
   text-transform: uppercase;
 `;
 
