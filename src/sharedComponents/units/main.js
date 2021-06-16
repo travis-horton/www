3b7780008f7a2d7b507a31a2e33ec7b0bbd62d29
kiddspazz <travis@travish.com>
2021-06-16 10:59:16 -0600

    Increase margin between Header and Content

--- src/sharedComponents/units/main.js
+++ src/sharedComponents/units/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Main = styled.div`
-  margin: 10px auto;
+  margin: 30px auto;
   max-width: 60%;
   min-width: 700px;
 `;
