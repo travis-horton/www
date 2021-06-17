913f2bc15f6c0951cfeed7966e0c27c0826d5831
kiddspazz <travis@travish.com>
2021-06-16 19:14:10 -0600

    Add ul to shared units

--- src/sharedComponents/units/index.js
+++ src/sharedComponents/units/index.js
@@ -1,11 +1,13 @@
 import { default as Main } from './main';
 import { default as StyledH1 } from './h1';
 import { default as StyledH2 } from './h2';
+import { default as StyledUl } from './ul';
 import { default as StyledA } from './a';
 
 export {
   Main,
   StyledH1,
   StyledH2,
+  StyledUl,
   StyledA,
 };
---
+++ src/sharedComponents/units/ul.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledUl = styled.ul`
+  list-style: none;
+  padding-left: 0;
+`;
+
+export default StyledUl;
