20f4ae21293953f89086b210348abb8bc215f097
kiddspazz <travis@travish.com>
2021-07-07 21:52:03 -0600

    add blog logic

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {
+  Route, Switch, Link, useRouteMatch
+} from 'react-router-dom';
 
+import './styles.css';
 import './neon-button.css';
 
-const Blog = () => (
-  <main style={{ textAlign: 'center' }}>
-    <a className='blog__neon-button' href="#">here&apos;s where i write stuff sometimes</a>
+import { TableOfContents } from './TableOfContents';
+
+const Blog = () => {
+  const match = useRouteMatch();
+  return (
+  <main>
+    <Switch>
+      <Route path={`${match.path}/contents`}>
+        <TableOfContents />
+      </Route>
+      <Route path={match.path}>
+        <div class='blog__button-container'>
+        <Link
+          className='blog__neon-button'
+          to={`${match.url}/contents`}
+        >
+          here&apos;s where i write stuff sometimes
+        </Link>
+        </div>
+      </Route>
+    </Switch>
   </main>
-);
+  );
+};
 
 export default Blog;
