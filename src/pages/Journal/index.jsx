cfedd183f99f57b4de3482316b87a20ce1bff2f1
kiddspazz <travis@travish.com>
2024-07-02 15:01:07 -0700

    Clean up journal page... it kind of works!

--- src/pages/Journal/index.jsx
+++ src/pages/Journal/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import journalData from './journal_data';
+import journalData from './utils/journal_data';
 
 import './styles.css';
 
@@ -7,37 +7,47 @@ const MsPerDay = (1000 * 60 * 60 * 24);
 const MY_BDAY = new Date(1983, 9, 22);
 const HEIGHT = 85;
 const WIDTH = 85;
+const today = new Date();
 
 const getHOffset = (date) => {
-  const weekDay = date.getDay()
-  return WIDTH * weekDay
-}
+  const weekDay = date.getDay();
+  return WIDTH * weekDay;
+};
 
 const getVOffset = (date) => {
-  const today = new Date();
-
-  const daysBetweenTodayAndDay = Math.floor(
-    (today - date)/MsPerDay
+  const mostRecentSunday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - today.getDay(),
   );
-
-  const weeksBetweenTodayAndDay = Math.floor(
-    ((daysBetweenTodayAndDay + 6 - today.getDay())/7)
+  const weeksBetweenTodayAndDay = Math.ceil(
+    (mostRecentSunday - date) / (7 * MsPerDay),
   );
 
   return weeksBetweenTodayAndDay * HEIGHT;
-}
+};
 
-const getMyDay = (date) => Math.floor((date - MY_BDAY)/(1000 * 60 * 60 * 24))
+const getMyDay = (date) => {
+  const jsDate = new Date(date);
+  const year = jsDate.getFullYear() - 2000;
+  const month = (jsDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = jsDate.getDate().toString().padStart(2, '0');;
+
+  const myDay = Math.floor((date - MY_BDAY) / (1000 * 60 * 60 * 24));
+
+  return `${year}${month}${day} - ${myDay}`;
+};
 
 const Day = (day) => {
+  const date = new Date(day.date);
   const divLocation = {
-    'top': getVOffset(day.date),
-    'left': getHOffset(day.date),
+    top: getVOffset(date),
+    left: getHOffset(date),
   };
 
-  let className = "journal__day";
+  let className = 'journal__day';
   className += ` mh${day.mental_health - 5}`;
-  if (day.s) className += " s";
+  if (parseInt(day.s)) className += " s";
   const mental_health = {
     5: '😁',
     4: '😀',
@@ -45,29 +55,27 @@ const Day = (day) => {
     2: '😐',
     1: '😞',
     0: '😭',
-  }
+  };
 
   return (
-    <div className={className} key={Number(day.date)} style={divLocation}>
+    <div className={className} key={Number(date)} style={divLocation}>
       <div className="day__day-number">
         <p>
-        {getMyDay(day.date)}
+          {getMyDay(date)}
         </p>
         <p className="smiley">
-        {mental_health[day.mental_health - 5]}
+          {mental_health[Math.floor(day.mental_health) - 5]}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Journal = () => (
   <main>
     <div className="journal">
       {
-        journalData.map(day =>
-          Day(day)
-        )
+        journalData.map((day) => Day(day))
       }
     </div>
   </main>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
+export { default as Blog } from './Blog';
+export { default as Contact } from './Contact';
 export { default as Home } from './Home';
+export { default as Journal } from './Journal';
 export { default as Piano } from './Piano';
 export { default as Programming } from './Programming';
-export { default as Contact } from './Contact';
-export { default as Blog } from './Blog';
